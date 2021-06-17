0f9f3a9a9442f1cc0b68763738a1650297e93eb2
pietvanzoen <hi@piet.me>
2021-06-17 09:08:21 +0200

    initial vue app

---
+++ vue/src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div className="app">
+    <header className="header">
+      <h1>Vue Todo App</h1>
+    </header>
+    <TodoList></TodoList>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  background-color: #282c34;
+  min-height: 100vh;
+  color: #ccc;
+}
+
+.app {
+  padding-top: 10rem;
+}
+
+.header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  animation: App-logo-spin infinite 20s linear;
+  height: 20vmin;
+  pointer-events: none;
+}
+
+.todo-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+}
+
+.todo {
+  display: flex;
+  align-items: center;
+  margin: 1rem 0;
+}
+
+.checkbox {
+  line-height: 1;
+  font-size: 1.4em;
+  width: 1.4em;
+  height: 1.4em;
+}
+
+.checkbox:checked + input {
+  text-decoration: line-through;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  line-height: 2rem;
+  width: 500px;
+}
+
+input {
+  border: none;
+  background: transparent;
+  color: white;
+  font-size: 1.4rem;
+  outline: none;
+  width: 100%;
+}
+
+.delete-button {
+  border: none;
+  appearance: none;
+  background: none;
+  color: white;
+  cursor: pointer;
+}
+</style>
---
+++ vue/src/components/TodoList.vue
@@ -0,0 +1,66 @@
+<template>
+  <form class="todo-list">
+    <ul>
+      <div v-for="(todo, index) in todos" :key="`todo-${index}`" class="todo">
+        <input
+          class="checkbox"
+          type="checkbox"
+          v-model="todo.done"
+          />
+        <input
+          type="text"
+          placeholder="Todo title"
+          v-model="todo.title"
+          />
+        <button
+          type="button"
+          class="delete-button"
+          @click="deleteTodo(index)"
+          >
+          X
+        </button>
+      </div>
+    </ul>
+    <button type="button" @click="addTodo">
+      + New todo
+    </button>
+
+    <pre>{{ printTodos }}</pre>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          done: false,
+          title: "Do laundry",
+        },
+        {
+          done: false,
+          title: "Pick up package",
+        },
+      ]
+    };
+  },
+  computed: {
+    printTodos() {
+      return JSON.stringify(this.todos, null, 2);
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        done: false,
+        title: ""
+      });
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+}
+
+</script>
